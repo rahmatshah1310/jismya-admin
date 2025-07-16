bdf711eb88b6180ef063eea87d0fa73ee917eae8
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-16 16:09:20 +0500

    feat: Adding provider.js

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,9 +1,15 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 
-export default function Providers({children}) {
+export default function Providers({ children }) {
   const [queryClient] = useState(() => new QueryClient());
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
