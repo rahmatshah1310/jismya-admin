e0a3487a16ab4870d3e561edac10f34817ca5ff6
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-16 16:09:56 +0500

    adding reorderbanner

--- src/components/Modal/ReorderBannerModal.jsx
+++ src/components/Modal/ReorderBannerModal.jsx
@@ -1,25 +1,44 @@
+'use client'
 import { useReorderBannerMutation } from "@/app/api/bannerApi";
 import { useState } from "react";
-import Modal from "react-modal"
+import Modal from "react-modal";
 import InputField from "../ui/InputField";
 import Button from "../ui/Button";
 
 export default function ReorderBannerModal({ banner, onClose }) {
-  if (!banner) return null; // ✅ clean early return
-  const [order, setOrder] = useState(banner?.order || 0);
-  const reorderBanner = useReorderBannerMutation();
-  console.log(order, "order.................................")
+  if (!banner) return null;
+  const [order, setOrder] = useState(banner?.order);
+  const { mutate, isLoading } = useReorderBannerMutation();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    reorderBanner.mutate({
-      id: banner._id,
-      data: { order: Number(order) }
-    });
+    e.preventDefault();
+    mutate(
+      {
+        id: banner._id,
+        data: { order: Number(order) }
+      },
+      {
+        onSuccess: () => {
+          onClose();
+        },
+        onError: (error) => {
+    if (error?.response?.data?.message) {
+      alert(error.response.data.message);
+    }
+        
+      }
+    }
+    );
   };
 
   return (
-    <Modal isOpen={true} ariaHideApp={false} onRequestClose={onClose} className="bg-white w-full flex flex-col md:max-w-md h-auto rounded-lg shadow-lg overflow-hidden outline-none" overlayClassName="fixed inset-0 bg-black/25 z-50 flex items-center justify-center px-4 py-8">
+    <Modal
+      isOpen={true}
+      ariaHideApp={false}
+      onRequestClose={onClose}
+      className="bg-white w-full flex flex-col md:max-w-md h-auto rounded-lg shadow-lg overflow-hidden outline-none"
+      overlayClassName="fixed inset-0 bg-black/25 z-50 flex items-center justify-center px-4 py-8"
+    >
       <form onSubmit={handleSubmit} className="p-4 space-y-4">
         <h2 className="text-2xl font-bold">Reorder Banner</h2>
         <InputField
@@ -29,10 +48,13 @@ export default function ReorderBannerModal({ banner, onClose }) {
           onChange={(e) => setOrder(Number(e.target.value))}
           className="border p-2 w-full"
         />
-        <Button type="submit" className="bg-green-500 text-white w-full">Save</Button>
-        <Button onClick={onClose} className="bg-red-500 text-white w-full">Cancel</Button>
+        <Button type="submit" disabled={isLoading} className="bg-green-500 text-white w-full">
+          {isLoading ? "Saving..." : "Save"}
+        </Button>
+        <Button onClick={onClose} type="button" className="bg-red-500 text-white w-full">
+          Cancel
+        </Button>
       </form>
     </Modal>
   );
 }
-
