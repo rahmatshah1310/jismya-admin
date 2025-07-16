1c1ff8b4868fa507b97e403d89b3970ccf77cf1d
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-16 11:29:50 +0500

    ading bannersevice

--- src/app/services/bannerService.js
+++ src/app/services/bannerService.js
@@ -1,12 +1,12 @@
 import { sendRequest } from "@/app/services/sendingRequests";
 
 // ✅ Create Banner (Admin)
-export const createBanner = async (data) => {
+export const createBanner = async (formData) => {
   try {
     const response = await sendRequest({
       method: "POST",
-      url: "/banners", // adjust to your API endpoint
-      data,
+      url: "/banners",
+      data: formData, // ✅ correct field
     });
     return response.data;
   } catch (error) {
@@ -28,6 +28,50 @@ export const getBanners = async () => {
     throw error;
   }
 };
+
+
+export const getSingleBanner = async (id) => {
+  try {
+    const response = await sendRequest({
+      method: "GET",
+      url: `/banners/${id}`,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("BannerService [getSingleBanner] error:", error);
+    throw error;
+  }
+};
+
+
+export const getBannersByDevice = async (deviceType) => {
+  try {
+    const response = await sendRequest({
+      method: "GET",
+      url: `/banners/device/${deviceType}`,
+    });
+    return response.data.data;
+  } catch (error) {
+    console.error(`BannerService [getBannersByDevice] error:`, error);
+    throw error;
+  }
+};
+
+
+export const reorderBanner = async (data) => {
+  try {
+    const response = await sendRequest({
+      method: "PUT", // or PATCH, based on your backend
+      url: `/banners/reorder`, 
+      data,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("BannerService [reorderBanner] error:", error);
+    throw error;
+  }
+};
+
 
 // ✅ Delete Banner (Admin)
 export const deleteBanner = async (id) => {
@@ -63,5 +107,7 @@ export const bannerService = {
   createBanner,
   getBanners,
   deleteBanner,
+  getBannersByDevice,
   updateBanner,
+  getSingleBanner
 };
