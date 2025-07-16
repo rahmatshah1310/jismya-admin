c1f200339d4a57fa293602e83c9a24b31b2050b1
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-17 00:25:13 +0500

    feat: Add bannertable

--- src/components/tables/BannerTable.jsx
+++ src/components/tables/BannerTable.jsx
@@ -1,12 +1,12 @@
 import Image from 'next/image';
 import Button from '@/components/ui/Button';
 
-export default function BannerTable({ title, banners, setSelectedBanner, setShowEditModal, deleteBanner, handleViewBanner,handleReorderBanner }) {
+export default function BannerTable({ title, banners,handleViewBanner,handleReorderBanner,handleDeleteBanner,handleEditBanner }) {
     return (
         <section className="p-4 sm:p-6 ">
             <h2 className="text-lg font-bold mb-2">{title}</h2>
-            <div className="block overflow-x-auto">
-                <table className="w-full table-auto border text-left">
+            <div className="block w-full overflow-x-auto">
+                <table className="w-full table-auto border text-center">
                     <thead>
                         <tr className="bg-gray-100">
                             <th className="p-2 min-w-[140px]">Image</th>
@@ -24,8 +24,8 @@ export default function BannerTable({ title, banners, setSelectedBanner, setShow
                                 <td className="p-2 min-w-[140px]">
                                     <Image src={banner.imageUrl} alt="banner" width={120} height={60} className="rounded-md object-cover" />
                                 </td>
-                                <td className="p-2 min-w-[200px] max-w-[200px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.heading}</td>
-                                <td className="p-2 min-w-[200px] max-w-[200px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.description}</td>
+                                <td className="p-2 min-w-[10px] max-w-[10px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.heading}</td>
+                                <td className="p-2 min-w-[100px] max-w-[100px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.description}</td>
                                 <td className="p-2 min-w-[120px]">{banner.deviceType}</td>
                                 <td className="p-2 min-w-[100px]">
                                     <span className={`font-semibold ${banner.isActive ? "text-green-500" : "text-red-500"}`}>
@@ -40,12 +40,11 @@ export default function BannerTable({ title, banners, setSelectedBanner, setShow
                                     >
                                         View
                                     </Button>
-                                    <Button onClick={() => { setSelectedBanner(banner); setShowEditModal(true); }} className="flex-1 px-3 py-2 bg-blue-500 text-white rounded text-sm">Edit</Button>
-                                    <Button onClick={() => deleteBanner.mutate(banner._id)} className="px-2 py-1 bg-red-500 text-white rounded">Delete</Button>
+                                    <Button onClick={() => { handleEditBanner(banner); }} className="px-2 py-1 bg-blue-500 text-white rounded ">Edit</Button>
                                     <Button onClick={() => handleReorderBanner(banner)} className="px-2 py-1 bg-yellow-500 text-white rounded">
                                         Reorder
                                     </Button>
-
+                                    <Button onClick={() => handleDeleteBanner(banner._id)} className="px-2 py-1 bg-red-500 text-white rounded">Delete</Button>
                                 </td>
                             </tr>
                         ))}
