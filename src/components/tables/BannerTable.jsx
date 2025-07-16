e370588ed09f892d1c79abee7de4659fa8a4c118
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-16 14:37:43 +0500

    feat: change bannertable

--- src/components/tables/BannerTable.jsx
+++ src/components/tables/BannerTable.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Button from '@/components/ui/Button';
 
-export default function BannerTable({ title, banners, setSelectedBanner, setShowEditModal, deleteBanner }) {
+export default function BannerTable({ title, banners, setSelectedBanner, setShowEditModal, deleteBanner, handleViewBanner,handleReorderBanner }) {
     return (
         <section className="p-4 sm:p-6 ">
             <h2 className="text-lg font-bold mb-2">{title}</h2>
@@ -33,16 +33,25 @@ export default function BannerTable({ title, banners, setSelectedBanner, setShow
                                     </span>
                                 </td>
                                 <td className="p-2 min-w-[80px]">{banner.order}</td>
-                                <td className="p-2 min-w-[200px] space-x-2">
+                                <td className="p-2 min-w-[350px] space-x-2">
+                                    <Button
+                                        onClick={() => handleViewBanner(banner._id)}
+                                        className="px-2 py-1 bg-green-500 text-white rounded"
+                                    >
+                                        View
+                                    </Button>
                                     <Button onClick={() => { setSelectedBanner(banner); setShowEditModal(true); }} className="flex-1 px-3 py-2 bg-blue-500 text-white rounded text-sm">Edit</Button>
                                     <Button onClick={() => deleteBanner.mutate(banner._id)} className="px-2 py-1 bg-red-500 text-white rounded">Delete</Button>
+                                    <Button onClick={() => handleReorderBanner(banner)} className="px-2 py-1 bg-yellow-500 text-white rounded">
+                                        Reorder
+                                    </Button>
+
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
-
         </section>
     )
 }
