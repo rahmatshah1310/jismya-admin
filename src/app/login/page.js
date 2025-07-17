d720d28c3e329f0883d05285bfb0989140dcf9a6
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-18 02:30:28 +0500

    add toastify

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,35 +5,40 @@ import Button from "@/components/ui/Button";
 import { useLoginMutation } from "../api/authApi";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export default function Login() {
   const router = useRouter();
   const loginMutation = useLoginMutation();
-  const { userData,setAuthData } = useAuth();
+  const { userData, setAuthData } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   useEffect(() => {
     if (loginMutation.status === "success") {
       setAuthData(loginMutation.data);
-      alert("Authentication Successful");
-    } else if (loginMutation.status === "error") {
-      const errorMessage =
-        loginMutation.error?.message || "An error occurred during authentication.";
-      alert(`Authentication failed!\n${errorMessage}`);
     }
-  }, [loginMutation.status, loginMutation.error, loginMutation.data, setAuthData]);
-
-
-  const onSubmit = (e) => {
-    e.preventDefault(); // ✅ Correct usage
-    loginMutation.mutate({ email, password });
+  }, [loginMutation.status, loginMutation.data, setAuthData]);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await loginMutation.mutateAsync({ email, password });
+      toast.success(res?.message || "Login Susscesful");
+      router.push("/");
+    } catch (error) {
+      toast.error(typeof error === "string" ? error : "Something went wrong.");
+    }
   };
 
   return (
-    <form onSubmit={onSubmit} className="flex items-center justify-center min-h-screen px-4">
+    <form
+      onSubmit={onSubmit}
+      className="flex items-center justify-center min-h-screen px-4"
+    >
       <div className="w-full sm:w-3/4 md:w-2/3 lg:w-2/5 font-roboto xl:w-1/4 p-6 sm:p-8 border-[var(--light-green)] border rounded-2xl space-y-4 shadow-md bg-white">
-        <h2 className="text-2xl font-bold text-center text-[var(--color-pink-500)]">Log In</h2>
+        <h2 className="text-2xl font-bold text-center text-[var(--color-pink-500)]">
+          Log In
+        </h2>
 
         <InputField
           type="email"
