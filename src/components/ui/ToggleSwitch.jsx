31b9da3cafcdfa9aeb1e8573029a4a703bfc2831
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-17 20:13:40 +0500

    add

---
+++ src/components/ui/ToggleSwitch.jsx
@@ -0,0 +1,39 @@
+// components/ui/ToggleSwitch.jsx
+
+import Button from "./Button";
+
+export default function ToggleSwitch({
+  isActive,
+  onToggle,
+  activeText = "Active",
+  inactiveText = "Inactive",
+}) {
+  return (
+    <Button
+      onClick={onToggle}
+      className={`relative flex items-center h-8 w-24 ml-15 rounded-full transition-colors duration-300 ${
+        isActive ? "bg-green-500" : "bg-red-500"
+      }`}
+    >
+      <span
+        className={`absolute left-3 text-sm font-semibold text-white transition-opacity duration-300 ${
+          isActive ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        {activeText}
+      </span>
+      <span
+        className={`absolute right-3 text-sm font-semibold text-white transition-opacity duration-300 ${
+          isActive ? "opacity-0" : "opacity-100"
+        }`}
+      >
+        {inactiveText}
+      </span>
+      <span
+        className={`absolute left-1 top-1 w-6 h-6 bg-white rounded-full shadow-md transform transition-transform duration-300 ${
+          isActive ? "translate-x-[64px]" : "translate-x-0"
+        }`}
+      />
+    </Button>
+  );
+}
