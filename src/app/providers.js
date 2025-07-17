f52ba40eb56fc89e3ab2f7b1bee94ebe2e5fde9e
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-17 20:12:55 +0500

    Add

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,10 +1,14 @@
-'use client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Modal from "react-modal";
+import { useEffect, useState } from "react";
 
 export default function Providers({ children }) {
   const [queryClient] = useState(() => new QueryClient());
+  useEffect(() => {
+    Modal.setAppElement("body");
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
