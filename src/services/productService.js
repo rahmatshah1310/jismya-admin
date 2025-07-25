efbc56f70da5dc7d09cbca8db1b72efc9667ede4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-25 23:31:31 +0500

    add

---
+++ src/services/productService.js
@@ -0,0 +1,170 @@
+import { sendRequest } from "@/services/sendingRequests";
+
+// POST /products
+export const createProduct = async (data) => {
+  const response = await sendRequest({
+    method: "POST",
+    url: "/products",
+    data,
+  });
+  return response.data;
+};
+
+// PUT /products/:id
+export const updateProduct = async (id, data) => {
+  const response = await sendRequest({
+    method: "PUT",
+    url: `/products/${id}`,
+    data,
+  });
+  return response.data;
+};
+
+// GET /products/:id
+export const getSingleProduct = async (id) => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/${id}`,
+  });
+  return response.data;
+};
+
+// GET /categories
+export const getAllCategories = async () => {
+  const response = await sendRequest({
+    method: "GET",
+    url: "/products/categories",
+  });
+  return response.data;
+};
+
+// GET /products
+export const getAllProducts = async () => {
+  const response = await sendRequest({
+    method: "GET",
+    url: "/products/all",
+  });
+  return response.data;
+};
+
+// GET /products/sizes
+export const getSizes = async () => {
+  const response = await sendRequest({
+    method: "GET",
+    url: "/products/sizes",
+  });
+  return response.data;
+};
+
+// GET /products/size/:size
+export const getProductsBySize = async (size) => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/size/${size}`,
+  });
+  return response.data;
+};
+
+// GET /products/category/:categoryName
+export const getProductsByCategory = async (categoryName) => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/category/${categoryName}`,
+  });
+  return response.data;
+};
+
+// POST /products/images
+export const addDescriptionImages = async (data) => {
+  const response = await sendRequest({
+    method: "POST",
+    url: `/products/images`,
+    data,
+  });
+  return response.data;
+};
+
+// DELETE /products/images/:id
+export const deleteDescriptionImage = async (id) => {
+  const response = await sendRequest({
+    method: "DELETE",
+    url: `/products/images/${id}`,
+  });
+  return response.data;
+};
+
+// PUT /products/order/:id
+export const updateProductOrder = async (id, data) => {
+  const response = await sendRequest({
+    method: "PUT",
+    url: `/products/order/${id}`,
+    data,
+  });
+  return response.data;
+};
+
+// GET /products/reviews/:id
+export const getProductReviews = async (id) => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/reviews/${id}`,
+  });
+  return response.data;
+};
+
+// POST /products/review
+export const createProductReview = async (data) => {
+  const response = await sendRequest({
+    method: "POST",
+    url: `/products/review`,
+    data,
+  });
+  return response.data;
+};
+
+// GET /products/sales/:id
+export const getProductSales = async (id) => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/sales/${id}`,
+  });
+  return response.data;
+};
+
+// GET /products/sales
+export const getAllSales = async () => {
+  const response = await sendRequest({
+    method: "GET",
+    url: `/products/sales`,
+  });
+  return response.data;
+};
+
+// POST /products/sale
+export const createSale = async (data) => {
+  const response = await sendRequest({
+    method: "POST",
+    url: `/products/sale`,
+    data,
+  });
+  return response.data;
+};
+
+export const productService = {
+  createProduct,
+  updateProduct,
+  getSingleProduct,
+  getAllCategories,
+  getAllProducts,
+  getSizes,
+  getProductsBySize,
+  getProductsByCategory,
+  addDescriptionImages,
+  deleteDescriptionImage,
+  updateProductOrder,
+  getProductReviews,
+  createProductReview,
+  getProductSales,
+  getAllSales,
+  createSale,
+};
