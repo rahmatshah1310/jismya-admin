b7c8b69aed189bb7a081122fda89d805a70b41a0
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-26 18:35:05 +0500

    add

--- src/components/Modal/DeleteProductModal.jsx
+++ src/components/Modal/DeleteProductModal.jsx
@@ -3,23 +3,23 @@
 import Modal from "react-modal";
 import Button from "../ui/Button";
 import { toast } from "react-toastify";
+import { useDeleteSingleProduct } from "@/app/api/productApi";
 
 export default function DeleteProductModal({ isOpen, onClose, productId }) {
-
-   const handleDelete = () => {
-    deleteMutation.mutate(banner, {
+  const deleteMutation = useDeleteSingleProduct();
+  const isLoading = deleteMutation.isPending;
+  const handleDelete = () => {
+    deleteMutation.mutate(productId, {
       onSuccess: (res) => {
-        toast.success(res?.message || "Banner deleted successfully!");
+        toast.success(res?.message || "Product deleted successfully!");
         onClose();
       },
       onError: (error) => {
         console.error("Delete error:", error);
-        toast.error(typeof error === "string" ? error : "Something went wrong.");
-      }
+        toast.error(typeof error === "string" && error);
+      },
     });
   };
-
-
 
   return (
     <Modal
@@ -27,18 +27,18 @@ export default function DeleteProductModal({ isOpen, onClose, productId }) {
       onRequestClose={onClose}
       ariaHideApp={false}
       contentLabel="Delete Product"
-      className="bg-white w-full max-w-md p-6 rounded-lg shadow-lg outline-none"
-      overlayClassName="fixed inset-0 bg-black/25 z-50 flex items-center justify-center"
+      className="bg-white w-full max-w-md p-6 rounded-lg shadow-lg outline-none text-center"
+      overlayClassName="fixed inset-0 bg-black/25 z-50 flex items-center justify-center p-4"
     >
       <h2 className="text-xl font-bold text-red-600">Are you sure?</h2>
       <p className="mt-4 text-gray-700">This action cannot be undone.</p>
 
-      <div className="mt-6 flex justify-end gap-4">
+      <div className="mt-6 flex justify-center gap-4">
         <Button onClick={onClose} className="bg-gray-200 hover:bg-gray-300 text-black">
           Cancel
         </Button>
         <Button onClick={handleDelete} className="bg-red-600 hover:bg-red-700 text-white">
-          🗑️ Delete
+          {isLoading ? "Deleting..." : "  🗑️ Delete"}
         </Button>
       </div>
     </Modal>
