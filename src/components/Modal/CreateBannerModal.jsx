4a2263bb941c8db44a293e3aeebce7f317341727
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-28 16:25:03 +0500

    add

--- src/components/Modal/CreateBannerModal.jsx
+++ src/components/Modal/CreateBannerModal.jsx
@@ -98,10 +98,10 @@ export default function CreateBannerModal({
             placeholder="Heading"
             className="border p-2 w-full"
           />
-          <InputField
-            value={form.description}
-            onChange={(e) => setForm({ ...form, description: e.target.value })}
-            placeholder="Description"
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageSelect}
             className="border p-2 w-full"
           />
 
@@ -109,18 +109,20 @@ export default function CreateBannerModal({
             Device Type:{" "}
             <span className="font-bold capitalize">{deviceType}</span>
           </div> */}
-
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageSelect}
-            className="border p-2 w-full"
-          />
         </div>
+        
+          <textarea
+            id="description"
+            name="description"
+            rows={3}
+            className="w-full rounded-lg border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onChange={(e) => setForm({ ...form, description: e.target.value })}
+            placeholder="Description"
+          ></textarea>
         <Button
           type="submit"
           disabled={isSubmitting}
-          className="w-full bg-green-500 text-white p-2 rounded"
+          className={isSubmitting?"text-gray-300 bg-green-500 p-2 rounded w-full":"w-full bg-green-500 text-white p-2 rounded "}
         >
           {isSubmitting ? "Creating..." : "Create"}
         </Button>
