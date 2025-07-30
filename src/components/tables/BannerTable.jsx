f48e7a20f7b17e978b105217f021406429e82967
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-07-31 01:58:15 +0500

    add

--- src/components/tables/BannerTable.jsx
+++ src/components/tables/BannerTable.jsx
@@ -1,16 +1,8 @@
 import Image from "next/image";
 import Button from "@/components/ui/Button";
-import ToggleSwitch from "../ui/ToggleSwitch";
+import StatusToggle from "../ui/common/StatusToggle";
 
-export default function BannerTable({
-  title,
-  banners,
-  handleViewBanner,
-  handleReorderBanner,
-  handleDeleteBanner,
-  handleEditBanner,
-  handleToggle,
-}) {
+export default function BannerTable({ title, banners, handleViewBanner, handleReorderBanner, handleDeleteBanner, handleEditBanner, handleToggle }) {
   return (
     <section>
       <h2 className="text-lg font-bold mb-2">{title}</h2>
@@ -31,26 +23,17 @@ export default function BannerTable({
             {banners?.map((banner) => (
               <tr key={banner._id} className="border-t text-center">
                 <td className="p-2 min-w-[140px]">
-                  <Image
-                    src={banner.imageUrl}
-                    alt="banner"
-                    width={120}
-                    height={10}
-                    className="rounded-md h-[80px] object-cover"
-                  />
+                  <Image src={banner.imageUrl} alt="banner" width={120} height={10} className="rounded-md h-[80px] object-cover" />
                 </td>
-                <td className="p-2 min-w-[10px] max-w-[10px] overflow-hidden whitespace-nowrap text-ellipsis">
-                  {banner.heading}
-                </td>
-                <td className="p-2 min-w-[100px] max-w-[100px] overflow-hidden whitespace-nowrap text-ellipsis">
-                  {banner.description}
-                </td>
+                <td className="p-2 min-w-[10px] max-w-[10px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.heading}</td>
+                <td className="p-2 min-w-[100px] max-w-[100px] overflow-hidden whitespace-nowrap text-ellipsis">{banner.description}</td>
                 <td className="p-2 min-w-[120px]">{banner.deviceType}</td>
 
                 <td className="p-2 min-w-[100px]">
-                  <ToggleSwitch
+                  <StatusToggle
+                    id={banner._id}
                     isActive={banner.isActive}
-                    onToggle={() => handleToggle(banner._id, banner.isActive)}
+                    mutationFn={({ id, isActive }) => handleToggle(id, isActive)} // must return a Promise
                     activeText="Active"
                     inactiveText="Inactive"
                   />
@@ -58,10 +41,7 @@ export default function BannerTable({
 
                 <td className="p-2 min-w-[80px]">{banner.order}</td>
                 <td className="p-2 min-w-[350px] space-x-2">
-                  <Button
-                    onClick={() => handleViewBanner(banner._id)}
-                    className="px-2 py-1 bg-green-500 text-white rounded"
-                  >
+                  <Button onClick={() => handleViewBanner(banner._id)} className="px-2 py-1 bg-green-500 text-white rounded">
                     View
                   </Button>
                   <Button
@@ -72,16 +52,10 @@ export default function BannerTable({
                   >
                     Edit
                   </Button>
-                  <Button
-                    onClick={() => handleReorderBanner(banner)}
-                    className="px-2 py-1 bg-yellow-500 text-white rounded"
-                  >
+                  <Button onClick={() => handleReorderBanner(banner)} className="px-2 py-1 bg-yellow-500 text-white rounded">
                     Reorder
                   </Button>
-                  <Button
-                    onClick={() => handleDeleteBanner(banner._id)}
-                    className="px-2 py-1 bg-red-500 text-white rounded"
-                  >
+                  <Button onClick={() => handleDeleteBanner(banner._id)} className="px-2 py-1 bg-red-500 text-white rounded">
                     Delete
                   </Button>
                 </td>
