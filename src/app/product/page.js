7a8b23dc2dccfc058a65333488d70fea1cc3e082
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-21 23:14:05 +0500

    add

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -3,12 +3,19 @@
 import { Suspense } from 'react'
 import Product from './Product'
 import { DashboardLayout } from '@/components/layout/dashboard-layout'
+import { ClipLoader } from 'react-spinners'
 
 export default function ProductPage() {
   return (
     <DashboardLayout>
       {' '}
-      <Suspense fallback={<div>Loading products...</div>}>
+      <Suspense
+        fallback={
+          <div>
+            <ClipLoader />
+          </div>
+        }
+      >
         <Product />
       </Suspense>
     </DashboardLayout>
