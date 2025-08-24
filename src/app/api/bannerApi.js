8876ec31ca067c78cebe84df2b07194bee3a8a42
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-24 22:15:04 +0500

    add

--- src/app/api/bannerApi.js
+++ src/app/api/bannerApi.js
@@ -1,87 +1,85 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { bannerService } from "@/services/bannerService";
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { bannerService } from '@/services/banner.service'
 
 // ✅ Create Banner
 export const useCreateBannerMutation = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: bannerService.createBanner,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["banners"] });
-      queryClient.invalidateQueries({ queryKey: ['banners-device'] });
-    }
-  });
-};
+      queryClient.invalidateQueries({ queryKey: ['banners'] })
+      queryClient.invalidateQueries({ queryKey: ['banners-device'] })
+    },
+  })
+}
 
 // ✅ Delete Banner
 export const useDeleteBannerMutation = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: bannerService.deleteBanner,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["banners"] });
-      queryClient.invalidateQueries({ queryKey: ['banners-device'] });
-    }
-  });
-};
+      queryClient.invalidateQueries({ queryKey: ['banners'] })
+      queryClient.invalidateQueries({ queryKey: ['banners-device'] })
+    },
+  })
+}
 
 // ✅ Update Banner
 export const useUpdateBannerMutation = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: ({ id, data }) => bannerService.updateBanner(id, data),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['banners-device'] });
-
-    }
-  });
-};
+      queryClient.invalidateQueries({ queryKey: ['banners-device'] })
+    },
+  })
+}
 
 // ✅ Reorder Banner
 export const useReorderBannerMutation = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
   return useMutation({
     mutationFn: ({ id, data }) => bannerService.reorderBanner(id, data),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['banners-device'] });
-    }
-  });
-};
+      queryClient.invalidateQueries({ queryKey: ['banners-device'] })
+    },
+  })
+}
 
 // ✅ Fetch All Banners
 export const useGetBanners = () =>
   useQuery({
-    queryKey: ["banners"],
+    queryKey: ['banners'],
     queryFn: bannerService.getBanners,
-  });
+  })
 
 // ✅ Get Single Banner
 export const useSingleBanner = (id) =>
   useQuery({
-    queryKey: ["banner", id],
+    queryKey: ['banner', id],
     queryFn: () => bannerService.getSingleBanner(id),
     enabled: !!id,
-  });
+  })
 
 // ✅ Get Category Banners
 export const useBannersByDevice = (deviceType) =>
   useQuery({
-    queryKey: ["banners-device", deviceType],
+    queryKey: ['banners-device', deviceType],
     queryFn: () => bannerService.getBannersByDevice(deviceType),
-    onSuccess: () => {
-    },
+    onSuccess: () => {},
     enabled: !!deviceType,
-  });
+  })
 
 export const useToggleStatus = () => {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
 
   return useMutation({
     mutationFn: ({ id, isActive }) =>
       bannerService.toggleBannerStatus(id, { isActive }),
 
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['banners-device'] });
-    }
-  });
-};
+      queryClient.invalidateQueries({ queryKey: ['banners-device'] })
+    },
+  })
+}
