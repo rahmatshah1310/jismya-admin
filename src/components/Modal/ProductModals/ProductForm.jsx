e9af84d5b7d2f46c322e901f61caac345ae7c977
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-24 22:17:00 +0500

    add

--- src/components/Modal/ProductModals/ProductForm.jsx
+++ src/components/Modal/ProductModals/ProductForm.jsx
@@ -112,7 +112,7 @@ export default function ProductForm({ isOpen, onClose }) {
               onChange={(e) =>
                 setFormData((prev) => ({ ...prev, categoryId: e.target.value }))
               }
-              className="border border-gray-800 rounded p-3 w-full bg-background"
+              className="border rounded p-3 w-full bg-background"
               required
             >
               <option value="" disabled>
@@ -135,7 +135,7 @@ export default function ProductForm({ isOpen, onClose }) {
               id="description"
               name="description"
               rows={3}
-              className="w-full rounded-lg border px-3 py-2 bg-transparent border-gray-800"
+              className="w-full rounded-lg border px-3 py-2 bg-transparent"
               value={formData.description}
               onChange={handleChange}
             />
