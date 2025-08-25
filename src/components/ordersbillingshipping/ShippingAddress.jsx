67d63f8534b2b8d585813c07a50bd587f69edd00
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-25 16:13:58 +0500

    add

---
+++ src/components/ordersbillingshipping/ShippingAddress.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const ShippingAddress = ({ order }) => {
+  return (
+    <div>
+      <div className="bg-background text-foreground p-6 rounded-lg border shadow-sm mb-6">
+        <h2 className="text-xl font-semibold mb-4">Shipping Address</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <label className="block text-sm font-medium text-foreground">
+              Name
+            </label>
+            <p className="text-foreground">{order.shippingAddress.name}</p>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-foreground">
+              Email
+            </label>
+            <p className="text-foreground">{order.shippingAddress.email}</p>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-foreground">
+              Phone
+            </label>
+            <p className="text-foreground">{order.shippingAddress.phone}</p>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-foreground">
+              Country
+            </label>
+            <p className="text-foreground">{order.shippingAddress.country}</p>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-foreground">
+              City
+            </label>
+            <p className="text-foreground">{order.shippingAddress.city}</p>
+          </div>
+          <div className="md:col-span-2">
+            <label className="block text-sm font-medium text-foreground">
+              Complete Address
+            </label>
+            <p className="text-foreground">
+              {order.billingAddress.completeAddress}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShippingAddress
