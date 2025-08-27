4922400a6231447aabc0f07f5da95d53a0850502
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-08-28 01:11:16 +0500

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressContentEditableWarning>
+      <body >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
